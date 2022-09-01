1ed7dba7812ad153f39cf9163c68705a7d1f6fdc
jose-engineer <jose-engineer@outlook.com>
2022-09-01 16:12:26 -0500

    Code cleaning

--- SumEvenNumbers/Program.cs
+++ SumEvenNumbers/Program.cs
@@ -12,27 +12,24 @@ namespace SumEvenNumbers
 
             Console.WriteLine(SumEvenNumbers(A));
         }
-
-        //public static int SumEvenNumbers(int[] input)
-        //{
-        //    int result = 0;
-
-        //    foreach (var item in input)
-        //    {
-        //        if (item % 2 == 0)
-        //        {
-        //            result += item;
-        //        }
-        //    }
-
-        //    return result;
-        //}
-
-        //Using LINQ
+        
         public static int SumEvenNumbers(int[] input)
         {
             return input.Where(p => p % 2 == 0).Sum();
             //return input.Where(p => p % 2 == 0).Sum(e => (long)e);
         }
+
+        public static int SumEvenNumbers2(int[] input) {
+            int result = 0;
+
+            foreach (var item in input) {
+                if (item % 2 == 0) {
+                    result += item;
+                }
+            }
+
+            return result;
+        }
+        
     }
 }
